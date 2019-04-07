a3a16c309becaa76c84a2a9ebd96f0cad7c61127
Achyut Meher Sai Velpua <achyut.meher.sai.velpula@autodesk.com>
2019-04-07 19:13:59 -0400

    Ada Boost Classifier

---
+++ outlier_terminator.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.read_csv('Final_Features_With_Titles')
+
+df = df[df['Helpful Votes'] < 100]
+
+df['Classes'] = pd.cut(df['Helpful Votes'], bins=[-1, 0, 20, 100], include_lowest=True, labels=['Useless', 'Helpful', 'Very Helpful'])
+df2 = pd.DataFrame({'Votes': df['Helpful Votes'], 'Label': df['Classes']})
+print(df2)
+
